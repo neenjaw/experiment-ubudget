f6ecaba159f06f05f6095487e7bb8c069b13dfa6
Tim Austin <tim@neenjaw.com>
2018-09-12 14:48:01 -0600

    test

--- createdb.sql
+++ createdb.sql
@@ -1,5 +1,5 @@
 create database ubudget default character set utf8 default collate utf8_bin;
 
--- commented out user create for outside machine access
+-- prevent user privileges for outside machine access
 -- GRANT ALL PRIVILEGES ON ubudget.* to ubuser@'%' IDENTIFIED BY 'ubuser';
 GRANT ALL PRIVILEGES ON ubudget.* to ubuser@'localhost' IDENTIFIED BY 'ubuser';
