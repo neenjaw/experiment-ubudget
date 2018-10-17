c7b6a184ce89ac3593ad7b8f79422add00141425
Tim Austin <tim@neenjaw.com>
2018-10-17 00:07:14 -0600

    auth routes started, need user migration

---
+++ db/migrations/20181017000553_new_user_fields.js
@@ -0,0 +1,13 @@
+
+// TODO: ADD fields:
+// -user_name
+// -user_email
+// -user_password
+
+exports.up = function(knex, Promise) {
+
+};
+
+exports.down = function(knex, Promise) {
+    
+};
--- server/routes/auth/index.js
+++ server/routes/auth/index.js
@@ -8,69 +8,130 @@ const bcrypt = require('bcryptjs');
 const VerifyToken = require('./VerifyToken');
 
 const express  = require('express');
-
-// ============================
-// Express Router Setup
-// ============================
-
 const router = express.Router();
+
+const User = require('../../models/user');
 
 // ============================
 // Auth Routes
 // ============================
 
 router.get('/me', VerifyToken, function(req, res, next) {
-    User.findById(req.userId, { password: 0 }, function (err, user) {
-        if (err) return res.status(500).send({
-            error: true,
-            message: 'There was a problem finding the user.'
+    // BOOKSHELF
+    User.where('user_id', req.userId)
+        .fetch()
+        .then(function(user) {
+            if (!user) return res.status(404).send({
+                error: true,
+                message: 'No user found.'
+            });
+    
+            res.status(200).send(user);
+        })
+        .catch(function(err) {
+            return res.status(500).send({
+                error: true,
+                message: 'There was a problem finding the user.'
+            });
         });
 
-        if (!user) return res.status(404).send({
-            error: true,
-            message: 'No user found.'
-        });
-
-        res.status(200).send(user);
-    });
+    // MONGOOSE
+    // User.findById(req.userId, { password: 0 }, function (err, user) {
+    //     if (err) return res.status(500).send({
+    //         error: true,
+    //         message: 'There was a problem finding the user.'
+    //     });
+    // 
+    //     if (!user) return res.status(404).send({
+    //         error: true,
+    //         message: 'No user found.'
+    //     });
+    // 
+    //     res.status(200).send(user);
+    // });
 });
 
 router.post('/register', (req, res) => {
-    const hashedPassword = bcrypt.hashSync(req.body.password, 8);
+    const hashedPassword = bcrypt.hashSync(req.body.userPassword, 8);
 
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: hashedPassword
-    },
-    function (err, user) {
-        if (err) return res.status(500).send('There was a problem registering the user.');
+    // TODO: get the default user auth state ID
+    const defaultUserAuthState = 1;
 
-        var token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-            expiresIn: 60 * 60 //expires in 1hr
+    const values = {
+        user_name: req.body.userName,
+        user_email: req.body.userEmail,
+        user_password: hashedPassword,
+        user_first_name: req.body.userFirstName,
+        user_last_name: req.body.userLastName,
+        user_authorization_state_id: defaultUserAuthState
+    };
+
+    User.create(values)
+        .save()
+        .then(function(user) {
+            var token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+                expiresIn: 60 * 60 //expires in 1hr
+            });
+    
+            res.status(200).send({ auth: true, token });
+        })
+        .catch(function(err) {
+            if (err) return res.status(500).send('There was a problem registering the user.');
         });
 
-        res.status(200).send({ auth: true, token });
-    });
+    // MONGOOSE
+    // User.create({
+    //     name: req.body.name,
+    //     email: req.body.email,
+    //     password: hashedPassword
+    // },
+    // function (err, user) {
+    //     if (err) return res.status(500).send('There was a problem registering the user.');
+
+    //     var token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    //         expiresIn: 60 * 60 //expires in 1hr
+    //     });
+
+    //     res.status(200).send({ auth: true, token });
+    // });
 });
 
 
 router.post('/login', (req, res) => {
 
-    User.findOne({ email: req.body.email }, (err, user) => {
-        if (err) return res.status(500).send('Error on the server.');
-        if (!user) return res.status(404).send('No user found.');
+    User.where('user_name', req.body.userName)
+        .fetch()
+        .then(function(user) {
+            if (!user) return res.status(404).send('No user found.');
 
-        const passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
-
-        if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
-
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-            expiresIn: 60 * 60 //expires in 1hr
+            const passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+    
+            if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
+    
+            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+                expiresIn: 60 * 60 //expires in 1hr
+            });
+    
+            res.status(200).send({ auth: true, token: token });    
+        })
+        .catch(function(err) {
+            if (err) return res.status(500).send('Error on the server.');
         });
 
-        res.status(200).send({ auth: true, token: token });
-    });
+    // User.findOne({ email: req.body.email }, (err, user) => {
+    //     if (err) return res.status(500).send('Error on the server.');
+    //     if (!user) return res.status(404).send('No user found.');
+    // 
+    //     const passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+    // 
+    //     if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
+    // 
+    //     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    //         expiresIn: 60 * 60 //expires in 1hr
+    //     });
+    // 
+    //     res.status(200).send({ auth: true, token: token });
+    // });
 });
 
 module.exports = router;
